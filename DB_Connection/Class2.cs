99d9042e1ca7e9a49636a4436680de0974671d29
Rolando <rolando.fcfm2020@gmail.com>
2021-02-11 23:59:20 -0600

    first commit

---
+++ DB_Connection/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Connection
+{
+    public class EmployeeFacade
+    {
+        public List<Employees> GetAllEmployees()
+        {
+
+            NORTHWNDEntities DataContext = new NORTHWNDEntities();
+            var employees = DataContext.Employees.ToList();
+
+            return employees;
+        }
+    }
+}
